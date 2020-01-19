6f1b0617f27a0426cc7eb8f56b964eadd06bddbe
IamPopov <ivantsar@gmail.com>
2020-01-18 18:45:45 -0700

    created a shell for employee constructor

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,21 @@
+class Employee {
+    constructor(name, id, title) {
+        this.name = name;
+        this.id = id;
+        this.title = title;
+    }
+    getName() {
+
+    }
+    getId() {
+
+    }
+    getEmail() {
+
+    }
+    getRole() {
+        return Employee;
+    }
+}
+
+module.exports = Employee;
